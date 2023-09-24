086bc000f3740314b9fe79de8722a9c68a40a726
Diego Gomez <df.gomez11@uniandes.edu.co>
2023-09-24 16:17:48 -0600

    Finally fixed bug in job

--- job-scripts/exp1_min_test.sh
+++ job-scripts/exp1_min_test.sh
@@ -23,7 +23,7 @@ ENV=${envs[$N_ENV]}
 B=${bs[$N_B]}
 LR=${lrs[$N_LR]}
 
-cd ~/projects/def-bowling/diegog/laplacian_dual_dynamics/
+cd ~/projects/def-mbowling/diegog/laplacian_dual_dynamics/
 module load apptainer
 
-parallel apptainer exec --nv -B /home -B $PWD:/pwd -B /project -B /scratch -B /localscratch -B $PROJECT_FOLDER --pwd /pwd ~/apptainer/lk_haiku_n.sif python3 train_laprepr.py "EXP1-T" --use_wandb --wandb_offline --save_dir ~/logs/laplacian_dual_dynamics/off --config_file $CONFIG --env_name {3} --seed {1} --barrier_initial_val {2} --lr_barrier_coefs {4} :::: $SEEDS ::: $B ::: $ENV ::: $LR
+parallel apptainer exec --nv -B /home -B $PWD:/pwd -B /project -B /scratch -B /localscratch -B $PROJECT_FOLDER --pwd /pwd ~/apptainer/lk_haiku_n.sif python3 train_laprepr.py "EXP1-T" --use_wandb --wandb_offline --config_file $CONFIG --env_name {3} --seed {1} --barrier_initial_val {2} --lr_barrier_coefs {4} :::: $SEEDS ::: $B ::: $ENV ::: $LR
