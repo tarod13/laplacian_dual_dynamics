aadb5798692b3bc110bf4d9e9db87c1dbed04900
Diego Gomez <df.gomez11@uniandes.edu.co>
2023-10-03 19:16:58 -0600

    Removed save_dir from wandb init

--- train_laprepr.py
+++ train_laprepr.py
@@ -61,13 +61,13 @@ def main(hyperparams):
 
     if hparam_yaml['use_wandb']:
         # Set wandb save directory
-        save_dir = os.getcwd()
-        os.makedirs(save_dir, exist_ok=True)
+        # save_dir = os.getcwd()
+        # os.makedirs(save_dir, exist_ok=True)
 
         # Initialize wandb logger
         logger = wandb.init(
             project='laplacian-encoder', 
-            dir=save_dir,
+            # dir=save_dir,
             config=hparam_yaml,
         )   
         # wandb_logger.watch(laplacian_encoder)   # TODO: Test overhead
